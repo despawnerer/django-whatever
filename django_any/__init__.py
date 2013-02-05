d0bf2b18f6466d61960e96784abe382e5781677b
Алексей <despawn@gmail.com>
2013-02-05 11:29:25 +0400

    Catch ImproperlyConfigured in __init__.py
    When setuptools imports this to get the version (I think), the whole thing starts importing django.db which imports django.conf, which complains about not having a SECRET_KEY. We don't give a shit.

--- django_any/__init__.py
+++ django_any/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 __version__ = (0, 2, 3, 'final', 0)
+
+from django.core.exceptions import ImproperlyConfigured
+
 try:
     from django_any.forms import any_form_field, any_form
     from django_any.models import any_field, any_model
-except ImportError:
+except ImportError, ImproperlyConfigured:
     pass
-
