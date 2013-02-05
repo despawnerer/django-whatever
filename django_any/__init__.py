82e8a856c194205384252e71e5dc42a3a1fdebee
Алексей <despawn@gmail.com>
2013-02-05 11:33:01 +0400

    Whoops, fix except.

--- django_any/__init__.py
+++ django_any/__init__.py
@@ -6,5 +6,5 @@ from django.core.exceptions import ImproperlyConfigured
 try:
     from django_any.forms import any_form_field, any_form
     from django_any.models import any_field, any_model
-except ImportError, ImproperlyConfigured:
+except (ImportError, ImproperlyConfigured):
     pass
